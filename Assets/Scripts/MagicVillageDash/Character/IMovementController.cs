b6a0f840872d244f63f9b511dda2b69f6cdd7f29
Ernesto Refugio Cabrera Cerón <ercc98@hotmail.com>
2025-12-13 16:51:23 -0600

    added ImovementController interface

---
+++ Assets/Scripts/MagicVillageDash/Character/IMovementController.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+namespace MagicVillageDash.Character
+{
+    public interface IMovementController
+    {
+        void TurnLeft();
+        void TurnRight();
+        void MovingSpeed(float speed);
+        void Crouch(bool isCrouching);
+        void Jump();
+        void Landing();
+    }
+}
