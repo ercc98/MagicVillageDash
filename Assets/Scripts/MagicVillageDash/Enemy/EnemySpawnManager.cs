7d6b89d0590439a58fb48f22596daad887ea1b45
Ernesto Refugio Cabrera Cerón <ercc98@hotmail.com>
2025-12-18 16:18:50 -0600

    random lane for spawn enemy

--- Assets/Scripts/MagicVillageDash/Enemy/EnemySpawnManager.cs
+++ Assets/Scripts/MagicVillageDash/Enemy/EnemySpawnManager.cs
@@ -6,17 +6,18 @@ using System;
 using ErccDev.Foundation.Core.Factories;
 using MagicVillageDash.Audio;
 using ErccDev.Foundation.Audio;
+using Random = UnityEngine.Random;
 
 namespace MagicVillageDash.Enemies
 {
     public sealed class EnemySpawnManager : MonoBehaviour, IEnemySpawner
     {
-        [SerializeField] private static WaitForSeconds _waitForSeconds1 = new(1f);
+        [SerializeField] private static WaitForSeconds _waitForSeconds1 = new(1.5f);
         [Header("Factory & Parent")]
         [SerializeField] private MonoBehaviour enemyFactoryProvider;
 
         [Header("Lanes")]
-        [SerializeField] private int initialLane = 0;
+        [SerializeField] private int numberOfLanes = 3;
         [SerializeField] private float laneWidth = 2.2f;
 
         [Header("Respawn")]
@@ -50,25 +51,25 @@ namespace MagicVillageDash.Enemies
         }
         public void Spawn()
         {
-            respawnRoutine = StartCoroutine(SpawnEnemyAfterDelay(respawnDelay));
+            respawnRoutine = StartCoroutine(SpawnEnemyAfterDelay(respawnDelay, Random.Range(0, numberOfLanes)));
         }
-        
-        IEnumerator SpawnEnemyAfterDelay(float respawnDelay)
+        IEnumerator SpawnEnemyAfterDelay(float respawnDelay, int lane)
         {
             yield return new WaitForSeconds(respawnDelay);
             
-            float positionX = laneWidth * (initialLane - 1);
+            float positionX = laneWidth * (lane - 1);
             spawnAreaParticleSystem.transform.position = new Vector3(positionX, 0.05f, 0);
             spawnAreaParticleSystem.Play();
             AudioManager.Instance?.Play("SpawnAreaEnemy", SoundCategory.SFX);
             yield return _waitForSeconds1;
-            OnStartSpawn?.Invoke(initialLane);
-            OnSpawned?.Invoke(Spawn(positionX));
+            OnStartSpawn?.Invoke(lane);
+            OnSpawned?.Invoke(Spawn(positionX, lane));
         }
 
-        public EnemyController Spawn(float positionX)
+        public EnemyController Spawn(float positionX, int lane)
         {
             EnemyController spawnedEnemy = enemyFactory.Spawn(new Vector3(positionX, 1, 0), Quaternion.identity);
+            spawnedEnemy.SelfLaneMover.SnapToLane(lane);
             AudioManager.Instance?.Play("SpawnEnemy", SoundCategory.SFX);
             return spawnedEnemy;
         }
--- Assets/Scripts/MagicVillageDash/Enemy/IEnemySpawner.cs
+++ Assets/Scripts/MagicVillageDash/Enemy/IEnemySpawner.cs
@@ -4,7 +4,6 @@ using MagicVillageDash.Enemy;
 public interface IEnemySpawner
 {
     void Spawn();
-
     event Action<EnemyController> OnSpawned;
     event Action<int> OnStartSpawn;
 }
