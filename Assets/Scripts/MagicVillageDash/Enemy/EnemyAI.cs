3c55d977a97b702ec5e86595deeda642e5cb8bac
Ernesto Refugio Cabrera Cerón <ercc98@hotmail.com>
2025-12-19 14:54:33 -0600

    logic for sensors with near, mid and far

--- Assets/Scripts/MagicVillageDash/Enemy/EnemyAI.cs
+++ Assets/Scripts/MagicVillageDash/Enemy/EnemyAI.cs
@@ -1,7 +1,9 @@
 using System;
+using System.Collections;
 using ErccDev.Foundation.Core.Gameplay;
 using MagicVillageDash.Character;
 using MagicVillageDash.Runner;
+using MagicVillageDash.World;
 using UnityEngine;
 
 namespace MagicVillageDash.Enemy
@@ -9,13 +11,21 @@ namespace MagicVillageDash.Enemy
     public class EnemyAI : MonoBehaviour
     {
         [Header("Dependencies")]
-        [SerializeField] private EnemySensors sensors;
+        [SerializeField] private EnemySensors nearSensor;
+        [SerializeField] private EnemySensors midSensor;
+        [SerializeField] private EnemySensors farSensor;
         [SerializeField] private MonoBehaviour characterControllerProvider;
-        private IMovementController movementController;
-        private ILaneMover laneMovement;
+        [SerializeField] protected MonoBehaviour gameSpeedProvider;
+        [SerializeField] private GameObject Arrows;
+        IMovementController movementController;
+        ILaneMover laneMovement;
+        IGameSpeedController gameSpeedController;
 
         [Header("Decision")]
         [SerializeField] private float startDelay = 2.0f;
+        [SerializeField] private float adviceTime = 0.20f;
+        [SerializeField] private float changeNearSensorPorcentaje = 0.3f;
+        [SerializeField] private float changeFarSensorPorcentaje = 0.7f;
         [Tooltip("How often to think (seconds). 0.15 - 0.30 is a good range.")]
         [SerializeField] private float thinkInterval = 0.2f;
 
@@ -26,29 +36,60 @@ namespace MagicVillageDash.Enemy
         private float nextThinkTime;
         private float nextMoveAllowedTime;
         private float nextMoveNoCoinsTime;
+        private Action TurnMethod;
 
         void Awake()
         {
-            if (!sensors) sensors = GetComponent<EnemySensors>();
             movementController = characterControllerProvider as IMovementController ?? GetComponent<IMovementController>();
             laneMovement = characterControllerProvider as ILaneMover ?? GetComponent<ILaneMover>();
-            if (!sensors) Debug.LogError("EnemyAI: Missing EnemySensors.", this);
+            gameSpeedController = gameSpeedProvider as IGameSpeedController ?? FindAnyObjectByType<GameSpeedController>(FindObjectsInactive.Exclude);
             if (movementController == null) Debug.LogError("EnemyAI: Missing IMovementController.", this);
-            
+
             nextThinkTime       = Time.time + thinkInterval     +   startDelay;
             nextMoveAllowedTime = Time.time + moveCooldown      +   startDelay;
             nextMoveNoCoinsTime = Time.time + timeWithoutCoins  +   startDelay;
+            Arrows.SetActive(false);
         }
 
         void OnEnable()
         {
-            sensors.TriggerHit += OnTriggetHit;
+            nearSensor.OnTriggerHit += OnTriggetHit;
             GameEvents.GameOver += OnGameOver;
+
+            if (gameSpeedController.CurrentSpeed < gameSpeedController.MaxSpeed * changeNearSensorPorcentaje)
+            {
+                //Debug.Log("Near");
+                nearSensor.gameObject.SetActive(true);
+                farSensor.gameObject.SetActive(false);
+                midSensor.gameObject.SetActive(false);
+
+                nearSensor.OnTriggerHit += OnTriggetHit;
+            }
+            else if (gameSpeedController.CurrentSpeed > gameSpeedController.MaxSpeed * changeFarSensorPorcentaje)
+            {
+                //Debug.Log("farSensor");
+                farSensor.gameObject.SetActive(true);
+                nearSensor.gameObject.SetActive(false);
+                midSensor.gameObject.SetActive(false);
+
+                farSensor.OnTriggerHit += OnTriggetHit;
+            }
+            else
+            {
+                //Debug.Log("midSensor");
+                midSensor.gameObject.SetActive(true);
+                farSensor.gameObject.SetActive(false);
+                nearSensor.gameObject.SetActive(false);
+                
+                midSensor.OnTriggerHit += OnTriggetHit;
+
+            }
         }
 
         void OnDisable()
         {
-            sensors.TriggerHit -= OnTriggetHit;
+            if (nearSensor != null) nearSensor.OnTriggerHit -= OnTriggetHit;
+            if (farSensor != null) farSensor.OnTriggerHit -= OnTriggetHit;
             GameEvents.GameOver -= OnGameOver;
         }
 
@@ -61,7 +102,7 @@ namespace MagicVillageDash.Enemy
             if (Time.time < nextThinkTime) return;
             nextThinkTime = Time.time + thinkInterval;
             if (Time.time < nextMoveNoCoinsTime) return;
-            Dodge();
+            //FindCoins();
         }
 
         private void OnTriggetHit(string tag)
@@ -71,6 +112,11 @@ namespace MagicVillageDash.Enemy
                 case "Coin": Stay(); break;
                 case "Hazard": Dodge(); break;
             }
+        }
+        private void FindCoins()
+        {
+            nextMoveNoCoinsTime = Time.time + timeWithoutCoins;
+            Dodge();
         }
 
         private void Stay()
@@ -89,15 +135,27 @@ namespace MagicVillageDash.Enemy
         private void TryMoveLeft()
         {
             if (Time.time < nextMoveAllowedTime) return;
-            movementController.TurnLeft();
-            nextMoveAllowedTime = Time.time + moveCooldown;
+            TurnMethod = movementController.TurnLeft;
+            Arrows.transform.localScale = new Vector3(-1, Arrows.transform.localScale.y, Arrows.transform.localScale.z);           
+            StartCoroutine(WaitForTurned());
         }
 
         private void TryMoveRight()
         {
             if (Time.time < nextMoveAllowedTime) return;
-            movementController.TurnRight();
+            TurnMethod = movementController.TurnRight;
+            Arrows.transform.localScale = new Vector3(1, Arrows.transform.localScale.y, Arrows.transform.localScale.z);
+            StartCoroutine(WaitForTurned());
+        }
+        
+        IEnumerator WaitForTurned()
+        {            
+            Arrows.SetActive(true);
+            yield return new WaitForSeconds(adviceTime);
+            TurnMethod();
             nextMoveAllowedTime = Time.time + moveCooldown;
+            yield return new WaitForSeconds(0.1f);
+            Arrows.SetActive(false);
         }
     }
 }
