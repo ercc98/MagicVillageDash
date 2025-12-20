5816abff504cc76cab97cdf94c851aa59fe6106d
Ernesto Refugio Cabrera Cerón <ercc98@hotmail.com>
2025-12-20 13:42:25 -0600

    Last Modification, works fine

--- Assets/Scripts/FirebaseScripts/FireBaseAnalytics.cs
+++ Assets/Scripts/FirebaseScripts/FireBaseAnalytics.cs
@@ -1,39 +1,73 @@
-using Firebase.Analytics;
 using Firebase.Extensions; 
-using Firebase; 
 using UnityEngine;
-
+namespace FireBaseScripts
+{
     public class FireBaseAnalytics : MonoBehaviour
     {
         public static FireBaseAnalytics Instance;
         private bool isFirebaseReady = false;
         private Firebase.FirebaseApp app;
-    private void Awake()
-    {
-        if (Instance != null && Instance != this)
+        private void Awake()
         {
-            Destroy(gameObject);
-            return;
-        }
-        Instance = this;
-        DontDestroyOnLoad(gameObject);
-    }
-        
-        //Firebase.FirebaseApp.CheckAndFixDependenciesAsync();
-        /*
-        Firebase.FirebaseApp.CheckAndFixDependenciesAsync().ContinueWithOnMainThread(task => {
-        var dependencyStatus = task.Result;
-        if (dependencyStatus == Firebase.DependencyStatus.Available) {
-            // Create and hold a reference to your FirebaseApp,
-            // where app is a Firebase.FirebaseApp property of your application class.
-            app = Firebase.FirebaseApp.DefaultInstance;
+            if (Instance != null && Instance != this)
+            {
+                Destroy(gameObject);
+                return;
+            }
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
 
-            // Set a flag here to indicate whether Firebase is ready to use by your app.
-        } else {
-            UnityEngine.Debug.LogError(System.String.Format(
-            "Could not resolve all Firebase dependencies: {0}", dependencyStatus));
-            // Firebase Unity SDK is not safe to use here.
+            Firebase.FirebaseApp.CheckAndFixDependenciesAsync().ContinueWithOnMainThread(task =>
+            {
+                var dependencyStatus = task.Result;
+                if (dependencyStatus == Firebase.DependencyStatus.Available)
+                {
+                    // Create and hold a reference to your FirebaseApp,
+                    // where app is a Firebase.FirebaseApp property of your application class.
+                    app = Firebase.FirebaseApp.DefaultInstance;
+                    isFirebaseReady = true;
+                    // Set a flag here to indicate whether Firebase is ready to use by your app.
+                }
+                else
+                {
+                    UnityEngine.Debug.LogError(System.String.Format(
+                    "Could not resolve all Firebase dependencies: {0}", dependencyStatus));
+                    // Firebase Unity SDK is not safe to use here.
+                }
+            });
         }
-        });
-        */
+
+        public void LogAppStart()
+        {
+            if (!isFirebaseReady) return;
+            Firebase.Analytics.FirebaseAnalytics.LogEvent("app_start");
+        }
+
+        public void LogStartPlaying()
+        {
+            if (!isFirebaseReady) return;
+            Firebase.Analytics.FirebaseAnalytics.LogEvent("start_playing");
+            
+        }
+
+        public void LogPlayerDied(int distance, int coins)
+        {
+            if (!isFirebaseReady) return;
+
+            var parameters = new System.Collections.Generic.List<Firebase.Analytics.Parameter>
+            {
+                new("distance", distance),
+                new("coins", coins)
+            };
+
+            Firebase.Analytics.FirebaseAnalytics.LogEvent("player_died", parameters.ToArray());
+
+        }
+
+        public void LogStopPlaying()
+        {
+            if (!isFirebaseReady) return;
+            Firebase.Analytics.FirebaseAnalytics.LogEvent("stop_playing");
+        }
     }
+}
